71b6cf6ff2d2beba6e2d5f7aecc5c704918aadf6
Ashwan1-yadav <ashwaniyadav888@gmail.com>
2026-06-30 11:31:34 +0530

    Migrate : Android/ios app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: "com.android.librarys",
+  appName: "LibraSys",
+  webDir: "dist"
+};
+
+export default config;
